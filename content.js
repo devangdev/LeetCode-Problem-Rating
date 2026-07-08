f9ae9a081abb3b358e3ebae7c5bbe8a166bbb82b
Devang <devangsdev@gmail.com>
2026-07-09 02:01:58 +0530

    Initial MVP: hover tooltip with local ratings

---
+++ content.js
@@ -0,0 +1,69 @@
+let ratings = {};
+
+// ---------- Create Tooltip ----------
+const tooltip = document.createElement("div");
+tooltip.className = "lc-tooltip";
+document.body.appendChild(tooltip);
+
+// ---------- Make Tooltip Follow Mouse ----------
+document.addEventListener("mousemove", (e) => {
+    tooltip.style.left = e.clientX + 20 + "px";
+    tooltip.style.top = e.clientY + 20 + "px";
+});
+
+// ---------- Initialize ----------
+async function init() {
+    try {
+        const url = chrome.runtime.getURL("data/ratings.json");
+
+        const response = await fetch(url);
+
+        ratings = await response.json();
+
+        console.log("Ratings Loaded:", ratings);
+
+        setupHoverEvents();
+    }
+    catch (err) {
+        alert("Failed to load ratings.json");
+        console.error(err);
+    }
+}
+
+// ---------- Hover Events ----------
+function setupHoverEvents() {
+
+    document.addEventListener("mouseover", (event) => {
+
+        const problem = event.target.closest('a[href^="/problems/"]');
+
+        if (!problem) return;
+
+        const info = ratings[problem.id];
+
+        if (!info) {
+            tooltip.innerHTML = `
+                <div>No contest data</div>
+            `;
+        } else {
+            tooltip.innerHTML = `
+                <div>🏆 ${info.contest}</div>
+                <div>📝 ${info.question}</div>
+                <div>⭐ ${info.rating}</div>
+            `;
+        }
+
+        tooltip.style.display = "block";
+    });
+
+    document.addEventListener("mouseout", (event) => {
+
+        const problem = event.target.closest('a[href^="/problems/"]');
+
+        if (!problem) return;
+
+        tooltip.style.display = "none";
+    });
+}
+
+init();
